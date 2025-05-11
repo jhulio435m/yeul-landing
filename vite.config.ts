48dca27cb8216c874665e22c761737016257d3a9
jhulio435m <76138183+jhulio435m@users.noreply.github.com>
2025-05-11 16:52:43 -0500

    xd

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/jhulio435m/yeul_web-page', 
+  base: '/yeul-landing/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
